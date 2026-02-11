c3a19f83ded19a35d7b4f8a9a25dc9af898dff15
mariyasaji-2023 <mariyasajichiramel.com>
2026-02-11 14:39:37 +0530

    fix: move types to dependencies

--- dist/middleware/auth.middleware.js
+++ dist/middleware/auth.middleware.js
@@ -6,16 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authMiddleware = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token)
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
         return res.status(401).json({ message: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
     try {
         const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     }
-    catch {
-        res.status(401).json({ message: "Invalid token" });
+    catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
     }
 };
 exports.authMiddleware = authMiddleware;
--- dist/server.js
+++ dist/server.js
@@ -7,5 +7,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app_1 = __importDefault(require("./app"));
 const db_1 = __importDefault(require("./config/db"));
+const PORT = process.env.PORT || 5000;
 (0, db_1.default)();
-app_1.default.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app_1.default.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
